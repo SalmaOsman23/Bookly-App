7c9b2cf66f55f19f6c98611a77014500d702e71a
Salma <salmaosmann21@gmail.com>
2024-03-24 00:21:00 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/core/utils/app_colors.dart';
+import 'package:bookly_app/features/splash_view/presentation/screens/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      home: SplashView(),
+    );
+  }
+}
+
